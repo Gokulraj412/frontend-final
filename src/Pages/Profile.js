eab5348bae4025a200cea99c81580cd52ab31087
Gokulraj412 <127767427+Gokulraj412@users.noreply.github.com>
2023-11-22 15:25:48 +0530

    Update Profile.js

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -11,7 +11,7 @@ function Profile(props) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:8000/api/v1/astrologer/getAstrologer/${id}`, {
+        const response = await fetch(`${process.env.REACT_APP_URL}/api/v1/astrologer/getAstrologer/${id}`, {
           method: 'GET',
         });
 
