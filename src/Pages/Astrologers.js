203d4519953689bc7269952da3c74761f2bf634f
Gokulraj412 <127767427+Gokulraj412@users.noreply.github.com>
2023-11-22 15:24:33 +0530

    Update Astrologers.js

--- src/Pages/Astrologers.js
+++ src/Pages/Astrologers.js
@@ -15,7 +15,7 @@ function Astrologers() {
 
     useEffect(() => {
         async function fetchData() {
-            let response = await fetch("http://localhost:8000/api/v1/astrologer/allAstrologers", {
+            let response = await fetch("${process.env.REACT_APP_URL}/api/v1/astrologer/allAstrologers", {
                 method: "GET",
             });
             // console.log(response);
