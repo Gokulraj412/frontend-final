67f67b3cb8e5bb94b8c79c302462428efffca68f
Gokulraj412 <127767427+Gokulraj412@users.noreply.github.com>
2023-11-22 15:23:25 +0530

    Update adminActions.js

--- src/Actions/adminActions.js
+++ src/Actions/adminActions.js
@@ -15,7 +15,7 @@ export const login = (email, password) => async (dispatch) => {
 
     try {
         dispatch(loginRequest())
-        const data = await axios.post(`http://localhost:8000/api/v1/admin/login`, { email, password });
+        const data = await axios.post(`${process.env.REACT_APP_URL}/api/v1/admin/login`, { email, password });
         console.log(data);
         dispatch(loginSuccess(data))
     } catch (error) {
@@ -35,7 +35,7 @@ export const register = (userData) => async (dispatch) => {
         dispatch(registerRequest())
 
 
-        const { data } = await axios.post(`http://127.0.0.1:8000/api/v1/admin/register`, userData);
+        const { data } = await axios.post(`${process.env.REACT_APP_URL}/api/v1/admin/register`, userData);
         dispatch(registerSuccess(data))
     } catch (error) {
         dispatch(registerFail(error.response.data.message))
